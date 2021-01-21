aee067213ae7be1d0458837217a6b320cc2d1f91
Maxx100 <rezonyou@gmail.com>
2021-01-21 22:26:41 +0300

    Base

--- Checkers-main/main.py
+++ Checkers-main/main.py
@@ -1,1 +1,11 @@
 # https://github.com/Maxx100/Checkers
+# https://github.com/Maxx100/Checkers
+# https://github.com/Maxx100/Checkers
+# https://github.com/Maxx100/Checkers
+# https://github.com/Maxx100/Checkers
+"""
+reulicgnirthjtyo;eris8rygictrh,gosryh9
+tyvjbmryhecrubgrefhserpvpy8esogyiwvht4ioseh
+prgicwro8gyoirehgciouhf8iwypieshomtverugt
+eprovhgo8syxrogh,cpxuephgiuprhivunilugveukg
+"""
