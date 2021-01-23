84bb07992e33288aeb41f1e427650ab130c67d76
Maxx100 <rezonyou@gmail.com>
2021-01-24 02:14:22 +0300

    Base

--- Checkers-main/main.py
+++ Checkers-main/main.py
@@ -13,40 +13,26 @@ class Menu:
 
 class Board:
     def __init__(self):
-        # Список экземпляров фигур
-        self.arr = []
-        self.board_sq = [0] * 64  # Клетки: 0 - пусто  1 - шашка  2 - шашка взята  3 - подсвет как возможный ход
-        # Заполнение списков
-        for k in range(1, 8, 2):
-            self.board_sq[k - 1] = 1
-            self.arr.append(Checker("w", k))
-            self.board_sq[k + 8] = 1
-            self.arr.append(Checker("w", k + 9))
-            self.board_sq[k + 15] = 1
-            self.arr.append(Checker("w", k + 16))
-
-            self.arr.append(Checker("b", k + 41))
-            self.board_sq[k + 40] = 1
-            self.arr.append(Checker("b", k + 48))
-            self.board_sq[k + 47] = 1
-            self.arr.append(Checker("b", k + 57))
-            self.board_sq[k + 56] = 1
+        # Список фигур
+        self.board_sq = []
+        for i in range(64):
+            self.board_sq.append(["e", 0])
+        # e - empty | w - white | b - black
+        # Клетки: 0 - пусто  1 - шашка  2 - шашка взята  3 - подсвет как возможный ход
+        # Заполнение
+        for k in range(0, 7, 2):
+            self.board_sq[k] = ["w", 1]
+            self.board_sq[k + 9] = ["w", 1]
+            self.board_sq[k + 16] = ["w", 1]
+            self.board_sq[k + 41] = ["b", 1]
+            self.board_sq[k + 48] = ["b", 1]
+            self.board_sq[k + 57] = ["b", 1]
         # Стандартные значения графики
         self.width = 8
         self.height = 8
         self.left = 10
         self.top = 10
         self.cell_size = 85
-
-    # Проверка, возможен ли данный ход
-    def can_it_move(self, coord, fig):
-        # Временное хранилище данных о фигуре
-        temp = fig.return_data()
-        # Если ход возможен...
-        if abs(coord - temp[1]) in [7, 9]:
-            if coord not in self.board_sq:
-                return True
-        return False
 
     # Рендер поля
     def render(self):
@@ -61,29 +47,29 @@ class Board:
                                                                  self.cell_size), 1)
 
         for i in range(len(self.board_sq)):
-            if self.board_sq[i] == 2:
+            if self.board_sq[i][1] == 2:
                 pygame.draw.rect(screen, pygame.Color("blue"), ((i % 8) * self.cell_size + self.top - 1,
-                                                                (i - 1) // 8 * self.cell_size + self.left - 1,
+                                                                i // 8 * self.cell_size + self.left - 1,
                                                                 self.cell_size - 2,
                                                                 self.cell_size - 2))
-            elif self.board_sq[i] == 3:
+            elif self.board_sq[i][1] == 3:
                 pygame.draw.circle(screen, pygame.Color("blue"), ((i % 8) * self.cell_size +
                                                                   self.top + self.cell_size // 2,
-                                                                  (i - 1) // 8 * self.cell_size +
+                                                                  i // 8 * self.cell_size +
                                                                   self.left + self.cell_size // 2), self.cell_size // 4)
 
         # Отрисовка фигур
-        for i in self.arr:
-            temp = i.return_data()
-            if temp[0] == "w":
-                r = "white"
-            else:
-                r = "black"
-            pygame.draw.circle(screen, pygame.Color(r), ((temp[1] % 8) * self.cell_size +
-                                                         self.top + self.cell_size // 2,
-                                                         (temp[1] - 1) // 8 * self.cell_size +
-                                                         self.left + self.cell_size // 2),
-                               self.cell_size // 2 - 3)
+        for i in range(len(self.board_sq)):
+            if self.board_sq[i][1] in [1, 2]:
+                if self.board_sq[i][0] == "w":
+                    r = "white"
+                else:
+                    r = "black"
+                pygame.draw.circle(screen, pygame.Color(r), ((i % 8) * self.cell_size +
+                                                             self.top + self.cell_size // 2,
+                                                             i // 8 * self.cell_size +
+                                                             self.left + self.cell_size // 2),
+                                   self.cell_size // 2 - 3)
 
     # Обработка нажатия на поле
     def sq_coor(self, pos):
@@ -92,47 +78,39 @@ class Board:
         if pos[0] > self.width - 1 or pos[1] > self.height - 1 or 0 > pos[0] or 0 > pos[1]:
             print("None")
         else:
-            if 2 in self.board_sq:
-                if self.board_sq[pos[0] + pos[1] * 8] == 1 or self.board_sq[pos[0] + pos[1] * 8] == 2:
-                    self.board_sq[self.board_sq.index(2)] = 1
-                    if 3 in self.board_sq:
-                        self.board_sq[self.board_sq.index(3)] = 0
-                        if 3 in self.board_sq:
-                            self.board_sq[self.board_sq.index(3)] = 0
-                else:
-                    for i in range(len(self.arr)):
-                        if self.board_sq[pos[0] + pos[1] * 8] == self.arr[i].return_data()[1]:
-                            self.board_sq[pos[0] + pos[1] * 8] = 1
-                            self.arr.pop(i)
-                            if SEQUENCE == "w":
-                                self.arr.append(Checker("w", pos[0] + pos[1] * 8))
-                                SEQUENCE = "b"
-                            else:
-                                self.arr.append(Checker("b", pos[0] + pos[1] * 8))
-                                SEQUENCE = "w"
-            else:
-                for i in self.arr:
-                    if (SEQUENCE, self.board_sq[pos[0] + pos[1] * 8] + 1) == i.return_data():
-                        self.board_sq[pos[0] + pos[1] * 8] = 2
-                        if SEQUENCE == "w":
-                            self.board_sq[pos[0] + pos[1] * 8 + 7] = 3
-                            self.board_sq[pos[0] + pos[1] * 8 + 9] = 3
-                        else:
-                            self.board_sq[pos[0] + pos[1] * 8 - 7] = 3
-                            self.board_sq[pos[0] + pos[1] * 8 - 9] = 3
+            if self.board_sq[pos[0] + pos[1] * 8][1] != 0 and self.board_sq[pos[0] + pos[1] * 8][0] == SEQUENCE:
+                is_moving = [False, 0]
+                for i in range(len(self.board_sq)):
+                    if self.board_sq[i][1] == 2:
+                        is_moving = [True, i]
                         break
+                if is_moving[0]:
+                    if self.board_sq[pos[0] + pos[1] * 8][1] == 3:
+                        self.board_sq[pos[0] + pos[1] * 8] = self.board_sq[is_moving[1]]
+                        self.board_sq[is_moving[1]] = ["e", 0]
+                    for i in range(len(self.board_sq)):
+                        if self.board_sq[i][1] == 2:
+                            self.board_sq[i][1] = 1
+                        elif self.board_sq[i][1] == 3:
+                            self.board_sq[i][1] = 0
+                else:
+                    if self.board_sq[pos[0] + pos[1] * 8][0] == "w":
+                        if pos[0] != 7 and self.board_sq[pos[0] + pos[1] * 8 + 9][1] != 1:
+                            self.board_sq[pos[0] + pos[1] * 8 + 9] = ["e", 3]
+                        if pos[0] != 0 and self.board_sq[pos[0] + pos[1] * 8 + 7][1] != 1:
+                            self.board_sq[pos[0] + pos[1] * 8 + 7] = ["e", 3]
+                    else:
+                        if pos[0] != 7 and self.board_sq[pos[0] + pos[1] * 8 - 9][1] != 1:
+                            self.board_sq[pos[0] + pos[1] * 8 - 9] = ["e", 3]
+                        if pos[0] != 0 and self.board_sq[pos[0] + pos[1] * 8 - 7][1] != 1:
+                            self.board_sq[pos[0] + pos[1] * 8 - 7] = ["e", 3]
+            else:
+                for i in range(len(self.board_sq)):
+                    if self.board_sq[i][1] == 2:
+                        self.board_sq[i][1] = 1
+                    elif self.board_sq[i][1] == 3:
+                        self.board_sq[i][1] = 0
             print(pos)
-
-
-# Класс шашки
-class Checker:
-    def __init__(self, color, coord):
-        self.color = color
-        self.coord = coord
-
-    # Запрос данных о фигуре
-    def return_data(self):
-        return self.color, self.coord
 
 
 if __name__ == '__main__':
