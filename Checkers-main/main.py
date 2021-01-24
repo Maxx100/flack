b47a1b7a59a02213d3a9bee798aece381219e64d
Maxx100 <rezonyou@gmail.com>
2021-01-24 18:26:42 +0300

    Base

--- Checkers-main/main.py
+++ Checkers-main/main.py
@@ -160,18 +160,24 @@ class Board:
                         is_moving = [True, i]
                         break
                 if is_moving[0]:
-                    if self.board_sq[pos[0] + pos[1] * 8] == [SEQUENCE, 1]:
-                        self.clear_board()
-                        self.check_mb_step(pos)
+                    if self.board_sq[pos[0] + pos[1] * 8][1] == 5:
+                        self.board_sq[pos[0] + pos[1] * 8] = self.board_sq[is_moving[1]]
+                        self.board_sq[is_moving[1]] = []
                     else:
-                        if self.board_sq[pos[0] + pos[1] * 8][1] == 3:
-                            self.board_sq[pos[0] + pos[1] * 8] = self.board_sq[is_moving[1]]
-                            self.board_sq[is_moving[1]] = ["e", 0]
-                            if SEQUENCE == "w":
-                                SEQUENCE = "b"
-                            else:
-                                SEQUENCE = "w"
-                        self.clear_board()
+                        if self.board_sq[pos[0] + pos[1] * 8] == [SEQUENCE, 1]:
+                            self.clear_board()
+                            self.check_mb_step(pos)
+                        else:
+                            if self.board_sq[pos[0] + pos[1] * 8][1] == 3:
+                                self.board_sq[pos[0] + pos[1] * 8] = self.board_sq[is_moving[1]]
+                                self.board_sq[is_moving[1]] = ["e", 0]
+                                if SEQUENCE == "w":
+                                    SEQUENCE = "b"
+                                else:
+                                    SEQUENCE = "w"
+                            self.clear_board()
+                elif self.board_sq[pos[0] + pos[1] * 8][1] == 5:
+                    self.clear_board()
                 else:
                     self.check_mb_step(pos)
             else:
